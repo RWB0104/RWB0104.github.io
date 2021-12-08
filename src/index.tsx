a47e21228615a238ca6ba39d8c3e8e73956ce35d
RWB0104 <psj2716@gmail.com>
2021-12-08 10:14:05 +0900

    임시 페이지

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hydrate, render } from 'react-dom';
-import './index.scss';
-import App from './App';
-import { RecoilRoot } from 'recoil';
-
-const root = document.querySelector('main');
-
-root?.hasChildNodes() ? hydrate(<RecoilRoot><App /></RecoilRoot>, root) : render(<RecoilRoot><App /></RecoilRoot>, root);
