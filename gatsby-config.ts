4e3d78bcfbdf9d6e0228ee58209b5aec5e5c06e4
박성진 <psj2716@gmail.com>
2023-06-09 16:43:58 +0900

    🔨 [UPDATE] 설정파일 갱신 - React 전역 설정 추가 - VSCode 프로젝트 설정 갱신

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,49 +1,43 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-	siteMetadata: {
-		siteUrl: `https://www.yourdomain.tld`,
-	},
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
 	plugins: [
-		"gatsby-plugin-sass",
-		"gatsby-plugin-image",
-		"gatsby-plugin-sitemap",
+		'gatsby-plugin-sass',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sitemap',
 		{
-			resolve: "gatsby-plugin-manifest",
-			options: {
-				icon: "src/images/icon.png",
-			},
+			options: { icon: 'src/images/icon.png' },
+			resolve: 'gatsby-plugin-manifest'
 		},
-		"gatsby-plugin-mdx",
-		"gatsby-plugin-sharp",
-		"gatsby-transformer-sharp",
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
 		{
-			resolve: "gatsby-source-filesystem",
+			__key: 'images',
 			options: {
-				name: "images",
-				path: "./src/images/",
+				name: 'images',
+				path: './src/images/'
 			},
-			__key: "images",
+			resolve: 'gatsby-source-filesystem'
 		},
 		{
-			resolve: "gatsby-source-filesystem",
+			__key: 'pages',
 			options: {
-				name: "pages",
-				path: "./src/pages/",
+				name: 'pages',
+				path: './src/pages/'
 			},
-			__key: "pages",
+			resolve: 'gatsby-source-filesystem'
 		},
 		{
-			resolve: "gatsby-plugin-google-gtag",
-			options: {
-				trackingIds: ["G-1YPNLPR0CQ"],
-			},
-		},
+			options: { trackingIds: [ 'G-1YPNLPR0CQ' ] },
+			resolve: 'gatsby-plugin-google-gtag'
+		}
 	],
+	siteMetadata: { siteUrl: 'https://www.yourdomain.tld' }
 };
 
 export default config;
